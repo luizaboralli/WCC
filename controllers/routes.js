6e69fcede3a490aca411a8e93b81ec993a87b1a6
matheuszeggio <zeggiomath@gmail.com>
2021-05-24 17:32:47 -0300

    commit aula 5

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,6 +1,33 @@
+const Agendamento = require('../models/Agendamentos');
 module.exports = app =>{   
    
-   app.get('/', (req, resp)=>{
-        resp.send("Servidor OK")
+    app.get('/agendamentos', (req, resp)=> {
+        Agendamento.listagem(resp);
     });
+
+    app.get('/agendamentos/:id', (req, resp) => {
+        const id = parseInt(req.params.id)
+        Agendamento.buscaPorId(id, resp)
+    });
+
+    app.post('/agendamentos', (req, resp)=> {
+        const agendamento = req.body;
+
+        Agendamento.inserir(agendamento, resp);
+        
+    });
+
+    app.put('/agendamentos/:id', (req, resp)=>{
+        const id = parseInt(req.params.id)
+        const agendamento = req.body;
+
+        Agendamento.alterar(id, agendamento, resp);
+    });
+
+    app.delete('/agendamentos/:id', (req, resp)=>{
+        const id = parseInt(req.params.id)
+        
+        Agendamento.remover(id, resp);
+    })
+
 };
---
+++ models/Agendamentos.js
@@ -0,0 +1,94 @@
+const moment = require('moment');
+const conexao = require('../infra/conexao');
+
+class Agendamento {
+
+    listagem(resp){
+        const sql = 'SELECT * FROM agendamentos';
+
+        conexao.query(sql, (error, results) => {
+            if(error) {
+                resp.status(400).json(error)
+            }
+            resp.status(201).json(results);
+        });
+    };
+
+    buscaPorId(id, resp){
+        const sql = `SELECT * FROM agendamentos WHERE id =?`
+        conexao.query(sql, id, (error, result) =>{
+            if(error) {
+                resp.status(400).json(error)
+            }
+
+            resp.status(201).json(result)
+        });
+    };
+    
+    alterar(id, valores, resp){
+        const sql = 'UPDATE agendamentos SET ? WHERE id = ?'
+        
+        if(valores.data_servico){
+            valores.data_servico = moment(valores.data_servico).format('YYYY-MM-DD');
+        }
+
+        conexao.query(sql, [valores, id], (error, result) => {
+            if(error) {
+                resp.status(400).json(error)
+            }
+            resp.status(200).json(result)
+        })
+    };
+
+    remover(id, resp) {
+        const sql = `DELETE FROM agendamentos WHERE id = ?`
+        
+        conexao.query(sql, id, (error, results)=> {
+            if(error){
+                resp.status(400).json(error)
+            }
+            resp.status(201).json({
+               mensagem: `Agendamento com ${id} removido com sucesso!` 
+            })
+        });
+    };
+
+    inserir(agendamento, resp) {
+        const sql = `INSERT INTO agendamentos SET ?`
+
+        const data_servico = moment(agendamento.data_servico).format('YYYY-MM-DD');
+        const data_agendamento = moment().format('YYYY-MM-DD');
+
+        const agendamentoComData = {...agendamento, data_agendamento, data_servico};
+        const ehDataValid = moment(agendamento.data_servico).isSameOrAfter(data_agendamento);
+        const ehNomeCliente = agendamento.nome_cliente.length > 2;
+
+        const validacoes =  [
+            {
+                nome: "data_servico",
+                valido: ehDataValid,
+                mensagem: "Data do agendamento deve ser igual ou superior a data de hoje"
+            },
+            {
+                nome: "nome_cliente",
+                valido: ehNomeCliente,
+                mensagem: "O nome do cliente deve ter pelo menos 3 digitos"
+            }
+        ];
+
+        const errors = validacoes.filter(campo => !campo.valido);
+
+        if(errors.length > 0) {
+            return resp.status(400).json(errors);
+        }
+
+        conexao.query(sql, agendamentoComData, (error, results) => {
+            if(error) {
+                throw error
+            };
+ 
+            console.log(results);
+        });
+    }
+}
+module.exports = new Agendamento;
